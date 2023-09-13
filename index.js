977e9b48a634664bd7cb3890ed23a2a2959fcabb
Abdulloh <12@gmail.com>
2023-09-13 20:30:48 +0500

    assss

---
+++ index.js
@@ -0,0 +1,41 @@
+// a:
+
+// var myWord = prompt('Введите свое имя');
+
+// console.log('Меня зовут ' + myWord);
+
+// var myAge = prompt('Введите свой возраст');
+ 
+// console.log('Мне ' + myAge);
+ 
+// var primer1 = prompt('Решите пример 6 + 3 = ?');
+
+// console.log('Пример 1: 6 + 3 = 9 ' + 'Ваш ответ ' + primer1);
+ 
+// var primer2 = prompt('Решите пример 20 - 5 = ?');
+ 
+// console.log('Пример 2: 20 - 5 = 15 ' + 'Ваш ответ ' + primer2);
+ 
+// var primer3 = prompt('Решите пример 15 * 2 = ?');
+ 
+// console.log('Пример 3: 15 * 2 = 30 ' + 'Ваш ответ ' + primer3);
+
+// var primer4 = prompt('Решите пример 8 / 4 = ?');
+ 
+// console.log('Пример 4: 8 / 4 = 2 ' + 'Ваш ответ ' + primer4);
+
+// var end = alert('Откройте консоль для просмотра');
+
+
+
+
+
+// b:
+
+var X = +prompt('Введите первое число');
+
+var Y = +prompt('Введите второе число');
+
+var Z = +prompt('Введите третье число');
+
+console.log('Среднее арифметическое : ' + ((X + Y + Z) / 3));
